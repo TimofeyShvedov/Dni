c43fab51e6e52d368048478e020c89f9da37303a
Тимофей <shvedov.tim30@gmail.com>
2025-03-23 19:00:58 +0300

    +

--- app_tw/views.py
+++ app_tw/views.py
@@ -7,17 +7,17 @@ from app_tw.models import DescriptionModel
 
 # Create your views here.
 def risof(request):
-    vse_streamers = DescriptionModel.objects.all().values_list("username",flat=True)
-    for streamer in vse_streamers:
-        url = f"https://twitchtracker.com/api/channels/summary/{streamer}"
-        answer = requests.get(url=url)
-        try:
-
-            subcribes=answer.json()["followers_total"]
-        except Exception:
-            continue
-        DescriptionModel.objects.filter(username=streamer).update(followers=subcribes)
-        time.sleep(3)
+    # vse_streamers = DescriptionModel.objects.all().values_list("username",flat=True)
+    # for streamer in vse_streamers:
+    #     url = f"https://twitchtracker.com/api/channels/summary/{streamer}"
+    #     answer = requests.get(url=url)
+    #     try:
+    #
+    #         subcribes=answer.json()["followers_total"]
+    #     except Exception:
+    #         continue
+    #     DescriptionModel.objects.filter(username=streamer).update(followers=subcribes)
+    #     time.sleep(3)
     return render(request, "Dome.html")
 
 class OtprListView(ListView):
